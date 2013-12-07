6db55f02fc0154d489cc5297611ec7e5a1525bf4
Andrey Zlo <huntedbox@gmail.com>
2013-12-07 20:19:58 +0600

    CKeditor added

---
+++ admin/static/js/ckeditor_config.js
@@ -0,0 +1,5 @@
+CKEDITOR.editorConfig = function( config ) {
+    //config.uiColor = '#AADC6E';
+    config.filebrowserBrowseUrl = '/browser/browse.php?type=Images';
+    config.filebrowserUploadUrl = '/uploader/upload.php?type=Files';
+};
