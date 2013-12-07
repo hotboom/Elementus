2e8366569377e239749a3546c13b4428ef5ed30b
Andrey Zlo <huntedbox@gmail.com>
2013-12-07 20:25:57 +0600

    CKeditor added

--- admin/static/js/ckeditor_config.js
+++ admin/static/js/ckeditor_config.js
@@ -1,5 +1,5 @@
 CKEDITOR.editorConfig = function( config ) {
     //config.uiColor = '#AADC6E';
-    config.filebrowserBrowseUrl = '/browser/browse.php?type=Images';
-    config.filebrowserUploadUrl = '/uploader/upload.php?type=Files';
+    config.filebrowserBrowseUrl = '/admin/plugins/kcfinder/browse.php?type=Images';
+    config.filebrowserUploadUrl = '/admin/plugins/kcfinder/upload.php?type=Files';
 };
